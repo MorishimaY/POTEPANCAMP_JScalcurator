b19304b6a599e5a6c7c4e69f1f40fc4e08c0a63d
EC2 Default User <ec2-user@ip-172-31-34-165.ec2.internal>
2023-01-30 05:19:21 +0000

    done

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let total = 0;          
+let inputData = "";     
+let calcOperator = "+";
+let dataFlg = 1;
+
+function doClear() {
+  total = 0;
+  inputData = "";
+  calcOperator = "+";
+  dataFlg = 1;
+  document.myForm.result.value = 0;
+}
+
+function doValue(number) {
+  dataFlg = 0;
+  inputData += number;
+  document.myForm.result.value = Number(inputData);
+}
+
+
+function doCalculate(data) {
+  if (dataFlg === 0 && calcOperator != '=') {
+    dataFlg = 1;
+    total = exeCalc(total, inputData, calcOperator);
+    inputData = "";
+    calcOperator = data;
+    document.myForm.result.value = total;
+    }
+  
+  if (dataFlg === '=') {
+    dataFlg = 1;
+    calcOperator = data;
+    inputData = "";
+  } else {
+    dataFlg = 1;
+    calcOperator = data;
+  }
+}
+
+function exeCalc(total, inputData, calcOperator) {
+  let tmpData = Number(inputData);
+  let tmpTotal = Number(total);
+  
+  if (calcOperator === "+") {
+    return tmpTotal + tmpData;
+  }
+  else if (calcOperator === "-") {
+    return tmpTotal - tmpData;
+  }
+  else if (calcOperator === "*") {
+    return tmpTotal * tmpData;
+  }
+  else if (calcOperator === "/") {
+    return tmpTotal / tmpData;
+  }
+}
